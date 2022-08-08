34ae681fc35f11b97d90537af7be3d1577b402a5
Sweenus <48658188+Sweenus@users.noreply.github.com>
2022-08-09 00:07:04 +1000

    changed comments

--- src/main/java/net/sweenus/simplyswords/SimplySwords.java
+++ src/main/java/net/sweenus/simplyswords/SimplySwords.java
@@ -5,9 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SimplySwords implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
