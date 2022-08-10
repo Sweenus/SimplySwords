83c2b3020ea571c5df3b5a217abc48329c4b7e5a
Sweenus <48658188+Sweenus@users.noreply.github.com>
2022-08-10 19:38:56 +1000

    Added additional swords Added effects on hit Added recipes

--- src/main/java/net/sweenus/simplyswords/item/custom/SlownessSwordItem.java
+++ src/main/java/net/sweenus/simplyswords/item/custom/SlownessSwordItem.java
@@ -1,7 +1,6 @@
 package net.sweenus.simplyswords.item.custom;
 
 
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -9,14 +8,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
-public class LevitationSwordItem extends SwordItem {
-    public LevitationSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+public class SlownessSwordItem extends SwordItem {
+    public SlownessSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
 
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        target.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 200, 2), attacker);
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 2), attacker);
         return super.postHit(stack, target, attacker);
     }
 
