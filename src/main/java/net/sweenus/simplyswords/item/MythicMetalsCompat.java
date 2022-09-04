136c08268bc9cde6a7d270ae2d77624f1cdc0b4a
Sweenus <48658188+Sweenus@users.noreply.github.com>
2022-09-04 18:29:24 +1000

    Mythic Metals Aquarium & Banglum added

--- src/main/java/net/sweenus/simplyswords/item/MythicMetalsCompat.java
+++ src/main/java/net/sweenus/simplyswords/item/MythicMetalsCompat.java
@@ -3,7 +3,9 @@ package net.sweenus.simplyswords.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.ToolMaterials;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -13,46 +15,137 @@ import net.sweenus.simplyswords.item.custom.*;
 public class MythicMetalsCompat {
 
     //Compat for Mythic Metals
+    static int rapier_modifier = 1;
+    static int sai_modifier = 3;
+    static int claymore_modifier = 2;
+    static int adamantite_modifier = 5;
+    static int aquarium_modifier = 2;
+    static int banglum_modifier = 2;
 
 
     //ADAMANTITE
-    public static final Item ADAMANTITE_LONGSWORD = registerItem( "mythicmetals_compat/adamantite_longsword",
-            new SwordItem(ToolMaterials.IRON, 3, -2.4f,
+    public static final Item ADAMANTITE_LONGSWORD = registerItem( "mythicmetals_compat/adamantite/adamantite_longsword",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2.4f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_TWINBLADE = registerItem( "mythicmetals_compat/adamantite_twinblade",
-            new SwordItem(ToolMaterials.IRON, 3, -1.7f,
+    public static final Item ADAMANTITE_TWINBLADE = registerItem( "mythicmetals_compat/adamantite/adamantite_twinblade",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -1.7f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
-    public static final Item ADAMANTITE_RAPIER = registerItem( "mythicmetals_compat/adamantite_rapier",
-            new SwordItem(ToolMaterials.IRON, 2, -1.6f,
-                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
-
-    public static final Item ADAMANTITE_KATANA = registerItem( "mythicmetals_compat/adamantite_katana",
-            new SwordItem(ToolMaterials.IRON, 3, -2f,
+    public static final Item ADAMANTITE_RAPIER = registerItem( "mythicmetals_compat/adamantite/adamantite_rapier",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier - rapier_modifier, -1.6f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_SAI = registerItem( "mythicmetals_compat/adamantite_sai",
-            new SwordItem(ToolMaterials.IRON, 0, -1.1f,
+    public static final Item ADAMANTITE_KATANA = registerItem( "mythicmetals_compat/adamantite/adamantite_katana",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_SPEAR = registerItem( "mythicmetals_compat/adamantite_spear",
-            new SwordItem(ToolMaterials.IRON, 3, -2.7f,
+    public static final Item ADAMANTITE_SAI = registerItem( "mythicmetals_compat/adamantite/adamantite_sai",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier - sai_modifier, -1.1f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_GLAIVE = registerItem( "mythicmetals_compat/adamantite_glaive",
-            new SwordItem(ToolMaterials.IRON, 3, -2.6f,
+    public static final Item ADAMANTITE_SPEAR = registerItem( "mythicmetals_compat/adamantite/adamantite_spear",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2.7f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_WARGLAIVE = registerItem( "mythicmetals_compat/adamantite_warglaive",
-            new SwordItem(ToolMaterials.IRON, 3, -2.2f,
+    public static final Item ADAMANTITE_GLAIVE = registerItem( "mythicmetals_compat/adamantite/adamantite_glaive",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2.6f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
 
-    public static final Item ADAMANTITE_CUTLASS = registerItem( "mythicmetals_compat/adamantite_cutlass",
-            new SwordItem(ToolMaterials.IRON, 3, -2f,
+    public static final Item ADAMANTITE_WARGLAIVE = registerItem( "mythicmetals_compat/adamantite/adamantite_warglaive",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2.2f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
-    public static final Item ADAMANTITE_CLAYMORE = registerItem( "mythicmetals_compat/adamantite_claymore",
-            new SwordItem(ToolMaterials.IRON, 5, -2.8f,
+
+    public static final Item ADAMANTITE_CUTLASS = registerItem( "mythicmetals_compat/adamantite/adamantite_cutlass",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier, -2f,
                     new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item ADAMANTITE_CLAYMORE = registerItem( "mythicmetals_compat/adamantite/adamantite_claymore",
+            new SwordItem(ToolMaterials.DIAMOND, adamantite_modifier + claymore_modifier, -2.8f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+
+    //AQUARIUM
+    public static final Item AQUARIUM_LONGSWORD = registerItem( "mythicmetals_compat/aquarium/aquarium_longsword",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2.4f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_TWINBLADE = registerItem( "mythicmetals_compat/aquarium/aquarium_twinblade",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -1.7f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+    public static final Item AQUARIUM_RAPIER = registerItem( "mythicmetals_compat/aquarium/aquarium_rapier",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier - rapier_modifier, -1.6f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_KATANA = registerItem( "mythicmetals_compat/aquarium/aquarium_katana",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_SAI = registerItem( "mythicmetals_compat/aquarium/aquarium_sai",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier - sai_modifier, -1.1f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_SPEAR = registerItem( "mythicmetals_compat/aquarium/aquarium_spear",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2.7f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_GLAIVE = registerItem( "mythicmetals_compat/aquarium/aquarium_glaive",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2.6f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_WARGLAIVE = registerItem( "mythicmetals_compat/aquarium/aquarium_warglaive",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2.2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_CUTLASS = registerItem( "mythicmetals_compat/aquarium/aquarium_cutlass",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier, -2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item AQUARIUM_CLAYMORE = registerItem( "mythicmetals_compat/aquarium/aquarium_claymore",
+            new SwordItem(ToolMaterials.DIAMOND, aquarium_modifier + claymore_modifier, -2.8f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+
+    //BANGLUM
+    public static final Item BANGLUM_LONGSWORD = registerItem( "mythicmetals_compat/banglum/banglum_longsword",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2.4f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_TWINBLADE = registerItem( "mythicmetals_compat/banglum/banglum_twinblade",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -1.7f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+    public static final Item BANGLUM_RAPIER = registerItem( "mythicmetals_compat/banglum/banglum_rapier",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier - rapier_modifier, -1.6f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_KATANA = registerItem( "mythicmetals_compat/banglum/banglum_katana",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_SAI = registerItem( "mythicmetals_compat/banglum/banglum_sai",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier - sai_modifier, -1.1f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_SPEAR = registerItem( "mythicmetals_compat/banglum/banglum_spear",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2.7f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_GLAIVE = registerItem( "mythicmetals_compat/banglum/banglum_glaive",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2.6f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_WARGLAIVE = registerItem( "mythicmetals_compat/banglum/banglum_warglaive",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2.2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_CUTLASS = registerItem( "mythicmetals_compat/banglum/banglum_cutlass",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier, -2f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+    public static final Item BANGLUM_CLAYMORE = registerItem( "mythicmetals_compat/banglum/banglum_claymore",
+            new SwordItem(ToolMaterials.DIAMOND, banglum_modifier + claymore_modifier, -2.8f,
+                    new FabricItemSettings().group(ModItemGroup.SIMPLYSWORDS)));
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(SimplySwords.MOD_ID, name), item);
