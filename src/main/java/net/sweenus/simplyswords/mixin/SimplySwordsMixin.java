095bcac0b86c85d4021184a6fd6da759fe07432a
Sweenus <48658188+Sweenus@users.noreply.github.com>
2022-09-27 20:36:04 +1000

    redundant emissive texture removed misc

--- src/main/java/net/sweenus/simplyswords/mixin/SimplySwordsMixin.java
+++ src/main/java/net/sweenus/simplyswords/mixin/SimplySwordsMixin.java
@@ -1,22 +1,18 @@
 package net.sweenus.simplyswords.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
 import net.sweenus.simplyswords.SimplySwords;
-import net.sweenus.simplyswords.item.ModItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		SimplySwords.LOGGER.info("Loaded SimplySwords Mixin");
-	}
+public class SimplySwordsMixin {
+	//@Inject(at = @At("HEAD"), method = "init()V")
+	//private void init(CallbackInfo info) {
+		//SimplySwords.LOGGER.info("Loaded SimplySwords Mixin");
+	//}
 }
 
 /*
