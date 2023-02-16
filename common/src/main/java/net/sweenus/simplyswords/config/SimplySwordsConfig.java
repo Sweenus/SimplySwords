38419bd9c56ff0ca3f4bf9fc80cfdcddc35d85a5
Sweenus <48658188+Sweenus@users.noreply.github.com>
2023-02-16 15:10:09 +1100

    Configuration options provided for the fine tuning of Unique weapon loot table injections

--- common/src/main/java/net/sweenus/simplyswords/config/SimplySwordsConfig.java
+++ common/src/main/java/net/sweenus/simplyswords/config/SimplySwordsConfig.java
@@ -13,6 +13,7 @@ public class SimplySwordsConfig {
     private static final HashMap<String, Float> GENERAL_OPTIONS = new LinkedHashMap<>();
     private static final HashMap<String, Float> FLOAT_OPTIONS = new LinkedHashMap<>();
     private static final HashMap<String, Float> WEAPON_OPTIONS = new LinkedHashMap<>();
+    private static final HashMap<String, Float> LOOT_OPTIONS = new LinkedHashMap<>();
 
     public static boolean getBooleanValue(String key) {
         if (!BOOLEAN_OPTIONS.containsKey(key)) {
@@ -40,6 +41,22 @@ public class SimplySwordsConfig {
             System.out.println(key);
         }
         return WEAPON_OPTIONS.get(key);
+    }
+
+    public static float getLootModifiers(String key) {
+        if (!LOOT_OPTIONS.containsKey(key)) {
+            System.out.println(key);
+            return 0f;
+        }
+        return LOOT_OPTIONS.get(key);
+    }
+
+    public static boolean getLootList (String id) {
+        if (!LOOT_OPTIONS.isEmpty()) {
+            System.out.println(id);
+            return LOOT_OPTIONS.toString().contains(id);
+        }
+        return false;
     }
 
     public static void init() {
@@ -221,6 +238,24 @@ public class SimplySwordsConfig {
         BOOLEAN_OPTIONS.put("enable_passive_particles", true);
         BOOLEAN_OPTIONS.put("compat_gobber_end_weapons_unbreakable", true);
 
+        LOOT_OPTIONS.put("minecraft:entities/wither", 0.05f);
+        LOOT_OPTIONS.put("minecraft:chests/ruined_portal", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_armorer", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_butcher", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_cartographer", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_desert_house", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_fisher", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_fletcher", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_mason", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_plains_house", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_savanna_house", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_shepard", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_snowy_house", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_taiga_house", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_tannery", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_temple", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_toolsmith", 0f);
+        LOOT_OPTIONS.put("minecraft:chests/village/village_weaponsmith", 0f);
 
         WEAPON_OPTIONS.put("longsword_positive_damage_modifier", 0f);
         WEAPON_OPTIONS.put("twinblade_positive_damage_modifier", 0f);
@@ -363,6 +398,11 @@ public class SimplySwordsConfig {
             BOOLEAN_OPTIONS.put(entry.getKey(), entry.getValue().getAsBoolean());
         }
 
+        json = Config.getJsonObject(Config.readFile(new File("config/simplyswords/loot_config.json5")));
+        for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+            LOOT_OPTIONS.put(entry.getKey(), entry.getValue().getAsFloat());
+        }
+
         json = Config.getJsonObject(Config.readFile(new File("config/simplyswords/general_config.json5")));
         for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
             GENERAL_OPTIONS.put(entry.getKey(), entry.getValue().getAsFloat());
@@ -395,6 +435,37 @@ public class SimplySwordsConfig {
         }
         config.append("}");
         Config.createFile("config/simplyswords/booleans.json5", config.toString(), overwrite);
+
+        config = new StringBuilder("{\n");
+        i = 0;
+        for (String key : LOOT_OPTIONS.keySet()) {
+            if (i == 0) {
+                config.append("// --------------------------------------------- LOOT CONFIGURATION ----------------------------------------------------------------- \n");
+                config.append("// This config allows for the tweaking of loot injection for UNIQUE weapons. \n");
+                config.append("// Standard iron > diamond gear, and Runic Tablets, are controlled by the loot modifiers in the general_config, not here.  \n");
+                config.append("// ---------------------------------------------------------------------------------------------------------------------------------- \n");
+                config.append("// --------------------------------------------- How does it work? ------------------------------------------------------------------ \n");
+                config.append("// If 'add_weapons_to_loot_tables' is enabled in the boolean config, Simply Swords will attempt to inject its loot into    \n");
+                config.append("// any loot table that contains 'chests' in its filepath. This includes loot tables from other mods.\n");
+                config.append("// Any values provided in this config will override the aforementioned process, acting as both a whitelist, and a blacklist.\n");
+                config.append("\n");
+                config.append("// Each line must be made up of a string containing the namespace, path, and filename, in addition to a float value.        \n");
+                config.append("// The float value provided will determine the chance of the loot appearing in the loot table. Where 0.0 is %0, and 1.0 is %100.\n");
+                config.append("// If the float value provided is 0.0 the items will be blacklisted from this loot table and skipped entirely by the loot injection.\n");
+                config.append("\n");
+                config.append("// Items can also be injected into entity loot tables, as seen in the Wither example below.\n");
+                config.append("// ---------------------------------------------------------------------------------------------------------------------------------- \n");
+                config.append("\n");
+            }
+            config.append("  \"").append(key).append("\": ").append(LOOT_OPTIONS.get(key));
+            ++i;
+            if (i < LOOT_OPTIONS.size()) {
+                config.append(",");
+            }
+            config.append("\n");
+        }
+        config.append("}");
+        Config.createFile("config/simplyswords/loot_config.json5", config.toString(), overwrite);
 
         config = new StringBuilder("{\n");
         i = 0;
--- common/src/main/java/net/sweenus/simplyswords/util/ModLootTableModifiers.java
+++ common/src/main/java/net/sweenus/simplyswords/util/ModLootTableModifiers.java
@@ -109,40 +109,74 @@ public class ModLootTableModifiers {
         }));
 
         //UNIQUE
+        // Check each loot table against the listed namespaces in the loot_config.json, if there's a match modify the
+        // table according to the config. Otherwise, use the loot global loot modifiers set in the general_config.json
         LootEvent.MODIFY_LOOT_TABLE.register(((lootTables, id, context, builtin) -> {
-            if (SimplySwordsConfig.getBooleanValue("add_weapons_to_loot_tables") && id.getPath().contains("chests")) {
-                if (!SimplySwordsConfig.getBooleanValue("loot_can_be_found_in_villages") && id.getPath().contains("village")) {
-                    //Do nothing
+            if (SimplySwordsConfig.getBooleanValue("add_weapons_to_loot_tables")) {
+                if (SimplySwordsConfig.getLootList(id.toString())) {
+                    float lootChance = SimplySwordsConfig.getLootModifiers(id.toString());
+                    if (lootChance > 0.0) { // If chance is set to 0 treat as a blacklist and don't inject the loot at all
+                        LootPool.Builder pool = LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(RandomChanceLootCondition.builder(lootChance)) // 1 = 100% of the time
+                                .with(ItemEntry.builder(ItemsRegistry.WATCHER_CLAYMORE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.TOXIC_LONGSWORD.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SWORD_ON_A_STICK.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.BRAMBLETHORN.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.STORMS_EDGE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.STORMBRINGER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.MJOLNIR.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.EMBERBLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.HEARTHFLAME.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.TWISTED_BLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULRENDER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULPYRE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULKEEPER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULSTEALER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.FROSTFALL.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.MOLTEN_EDGE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.LIVYATAN.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.ICEWHISPER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.ARCANETHYST.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.THUNDERBRAND.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.BRIMSTONE_CLAYMORE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SLUMBERING_LICHBLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SHADOWSTING.get()));
+
+                        context.addPool(pool);
+                    }
                 }
                 else {
-                    LootPool.Builder pool = LootPool.builder()
-                            .rolls(ConstantLootNumberProvider.create(1))
-                            .conditionally(RandomChanceLootCondition.builder(SimplySwordsConfig.getGeneralSettings("unique_loot_table_weight"))) // 1 = 100% of the time
-                            .with(ItemEntry.builder(ItemsRegistry.WATCHER_CLAYMORE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.TOXIC_LONGSWORD.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SWORD_ON_A_STICK.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.BRAMBLETHORN.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.STORMS_EDGE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.STORMBRINGER.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.MJOLNIR.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.EMBERBLADE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.HEARTHFLAME.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.TWISTED_BLADE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SOULRENDER.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SOULPYRE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SOULKEEPER.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SOULSTEALER.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.FROSTFALL.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.MOLTEN_EDGE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.LIVYATAN.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.ICEWHISPER.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.ARCANETHYST.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.THUNDERBRAND.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.BRIMSTONE_CLAYMORE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SLUMBERING_LICHBLADE.get()))
-                            .with(ItemEntry.builder(ItemsRegistry.SHADOWSTING.get()));
+                    if (id.getPath().contains("chests")) {
+                        LootPool.Builder pool = LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(RandomChanceLootCondition.builder(SimplySwordsConfig.getGeneralSettings("unique_loot_table_weight"))) // 1 = 100% of the time
+                                .with(ItemEntry.builder(ItemsRegistry.WATCHER_CLAYMORE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.TOXIC_LONGSWORD.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SWORD_ON_A_STICK.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.BRAMBLETHORN.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.STORMS_EDGE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.STORMBRINGER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.MJOLNIR.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.EMBERBLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.HEARTHFLAME.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.TWISTED_BLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULRENDER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULPYRE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULKEEPER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SOULSTEALER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.FROSTFALL.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.MOLTEN_EDGE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.LIVYATAN.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.ICEWHISPER.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.ARCANETHYST.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.THUNDERBRAND.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.BRIMSTONE_CLAYMORE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SLUMBERING_LICHBLADE.get()))
+                                .with(ItemEntry.builder(ItemsRegistry.SHADOWSTING.get()));
 
-                    context.addPool(pool);
+                        context.addPool(pool);
+                    }
                 }
             }
         }));
