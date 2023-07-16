e681608f09207ef7f261e3e4abb09935c6219065
Sweenus <48658188+Sweenus@users.noreply.github.com>
2023-07-16 13:50:49 +1000

    Re-added Patchouli support Fixed broken Runic Sai recipe Adjusted Unique weapons to not show socket tooltips when no sockets are available

--- common/src/main/java/net/sweenus/simplyswords/item/UniqueSwordItem.java
+++ common/src/main/java/net/sweenus/simplyswords/item/UniqueSwordItem.java
@@ -430,6 +430,9 @@ public class UniqueSwordItem extends SwordItem {
 
             }
 
-        } else {tooltip.add(Text.translatable("item.simplyswords.common.showtooltip").formatted(Formatting.GRAY));}
+        } else if (itemStack.getOrCreateNbt().getString("runic_power").contains("socket_empty") ||
+                itemStack.getOrCreateNbt().getString("nether_power").contains("socket_empty")) {
+            tooltip.add(Text.translatable("item.simplyswords.common.showtooltip").formatted(Formatting.GRAY));
+        }
     }
 }
