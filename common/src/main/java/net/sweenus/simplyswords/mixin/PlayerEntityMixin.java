154a2ed1ba5acc1ec80905e0f83744d39b459294
Sweenus <48658188+Sweenus@users.noreply.github.com>
2025-09-03 14:42:50 +1000

    Changes - Axolotls are now removed from the player shoulder if no relevant item is in inventory

--- common/src/main/java/net/sweenus/simplyswords/mixin/PlayerEntityMixin.java
+++ common/src/main/java/net/sweenus/simplyswords/mixin/PlayerEntityMixin.java
@@ -2,9 +2,12 @@ package net.sweenus.simplyswords.mixin;
 
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import net.sweenus.simplyswords.registry.ItemsRegistry;
+import net.sweenus.simplyswords.util.HelperMethods;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,11 +19,19 @@ public abstract class PlayerEntityMixin {
 
     @Shadow @Final private PlayerAbilities abilities;
 
+    @Unique
+    public void simplySwords$invokeDropShoulderEntities() {
+        dropShoulderEntities();
+    }
+
     @Inject(at = @At("HEAD"), method = "dropShoulderEntities", cancellable = true)
     public void simplyswords$dropShoulderEntities(CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity) (Object) this;
+
+        if (!HelperMethods.hasItemInInventory(player, ItemsRegistry.CHOMPOLOTL.get())) return;
+
         // Control how shoulder axolotl are dropped
-        if (!player.isTouchingWater() && !player.isSneaking() && !player.isSleeping() && !player.inPowderSnow && !this.abilities.flying) {
+        if (!player.isTouchingWater() && !player.isSneaking() && !player.isSleeping() && !player.inPowderSnow && !this.abilities.flying && player.isAlive()) {
 
             if (player.getShoulderEntityLeft() != null && "simplyswords:simplyaxolotlentity".equals(player.getShoulderEntityLeft().getString("id"))) {
                 ci.cancel();
@@ -28,6 +39,17 @@ public abstract class PlayerEntityMixin {
 
             if (player.getShoulderEntityRight() != null && "simplyswords:simplyaxolotlentity".equals(player.getShoulderEntityRight().getString("id"))) {
                 ci.cancel();
+            }
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "tick")
+    public void simplyswords$tick(CallbackInfo ci) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        if (player.age % 40 == 0) {
+            // Drop axolotls if Chompolotl item not present
+            if (!HelperMethods.hasItemInInventory(player, ItemsRegistry.CHOMPOLOTL.get())) {
+                dropShoulderEntities();
             }
         }
     }
