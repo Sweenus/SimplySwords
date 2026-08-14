2ae5a0122d0aa6334c84d20adc84990e924b60df
Sweenus <sweeney.a.james@gmail.com>
2026-08-14 22:53:49 +1000

    Small changes - Update tooltip bulk exporter - Update doc tooltip images

--- common/src/main/java/net/sweenus/simplyswords/client/tooltip/UniqueTooltipExportController.java
+++ common/src/main/java/net/sweenus/simplyswords/client/tooltip/UniqueTooltipExportController.java
@@ -5,7 +5,10 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.sweenus.simplyswords.api.AwakeningApi;
+import net.sweenus.simplyswords.api.AwakeningFormRegistry;
+import net.sweenus.simplyswords.item.component.AwakeningRouteComponent;
 import net.sweenus.simplyswords.power.GemPowerComponent;
 import net.sweenus.simplyswords.registry.ComponentTypeRegistry;
 import net.sweenus.simplyswords.registry.GemPowerRegistry;
@@ -38,7 +41,7 @@ public final class UniqueTooltipExportController {
             spec("enigma", ItemsRegistry.ENIGMA::get),
             spec("flamewind", ItemsRegistry.FLAMEWIND::get),
             spec("frostfall", ItemsRegistry.FROSTFALL::get),
-            spec("harbinger", ItemsRegistry.HARBINGER::get),
+            spec("harbinger", ItemsRegistry.HARBINGER::get, AwakeningFormRegistry.HARBINGER_ROUTE),
             spec("hearthflame", ItemsRegistry.HEARTHFLAME::get),
             spec("hiveheart", ItemsRegistry.HIVEHEART::get),
             spec("icewhisper", ItemsRegistry.ICEWHISPER::get),
@@ -50,7 +53,7 @@ public final class UniqueTooltipExportController {
             spec("mjolnir", ItemsRegistry.MJOLNIR::get),
             spec("molten_edge", ItemsRegistry.MOLTEN_EDGE::get),
             spec("ribboncleaver", ItemsRegistry.RIBBONCLEAVER::get),
-            spec("righteous_relic", ItemsRegistry.RIGHTEOUS_RELIC::get, 4),
+            spec("righteous_relic", ItemsRegistry.RIGHTEOUS_RELIC::get, 4, AwakeningFormRegistry.SUN_ROUTE),
             spec("shadowsting", ItemsRegistry.SHADOWSTING::get),
             spec("soulkeeper", ItemsRegistry.SOULKEEPER::get),
             spec("soulpyre", ItemsRegistry.SOULPYRE::get),
@@ -59,21 +62,23 @@ public final class UniqueTooltipExportController {
             spec("stars_edge", ItemsRegistry.STARS_EDGE::get),
             spec("stormbringer", ItemsRegistry.STORMBRINGER::get),
             spec("stormscale", ItemsRegistry.STORMSCALE::get),
-            spec("ionbound_stormscale", ItemsRegistry.IONBOUND_STORMSCALE::get),
+            spec("ionbound_stormscale", ItemsRegistry.IONBOUND_STORMSCALE::get,
+                    AwakeningFormRegistry.IONBOUND_ROUTE),
             spec("storms_edge", ItemsRegistry.STORMS_EDGE::get),
-            spec("sunfire", ItemsRegistry.SUNFIRE::get),
-            spec("tainted_relic", ItemsRegistry.TAINTED_RELIC::get, 4),
+            spec("sunfire", ItemsRegistry.SUNFIRE::get, AwakeningFormRegistry.SUN_ROUTE),
+            spec("tainted_relic", ItemsRegistry.TAINTED_RELIC::get, 4, AwakeningFormRegistry.HARBINGER_ROUTE),
             spec("tempest", ItemsRegistry.TEMPEST::get),
             spec("thunderbrand", ItemsRegistry.THUNDERBRAND::get),
             spec("toxic_longsword", ItemsRegistry.TOXIC_LONGSWORD::get),
             spec("twisted_blade", ItemsRegistry.TWISTED_BLADE::get),
             spec("watcher_claymore", ItemsRegistry.WATCHER_CLAYMORE::get),
+            spec("the_devourer", ItemsRegistry.THE_DEVOURER::get, AwakeningFormRegistry.DEVOURER_ROUTE),
             spec("watching_warglaive", ItemsRegistry.WATCHING_WARGLAIVE::get),
             spec("waxweaver", ItemsRegistry.WAXWEAVER::get),
             spec("whisperwind", ItemsRegistry.WHISPERWIND::get),
             spec("wickpiercer", ItemsRegistry.WICKPIERCER::get),
             spec("wraithfang", ItemsRegistry.WRAITHFANG::get),
-            spec("wraithmaw", ItemsRegistry.WRAITHMAW::get)
+            spec("wraithmaw", ItemsRegistry.WRAITHMAW::get, AwakeningFormRegistry.WRAITHMAW_ROUTE)
     );
 
     public static int start() {
@@ -114,6 +119,10 @@ public final class UniqueTooltipExportController {
 
     private static TooltipExportEntry createEntry(ExportSpec spec) {
         ItemStack stack = new ItemStack(spec.item().get());
+        if (spec.awakeningRoute() != null) {
+            stack.set(ComponentTypeRegistry.AWAKENING_ROUTE.get(),
+                    new AwakeningRouteComponent(spec.awakeningRoute()));
+        }
         AwakeningApi.ensureInitialized(stack);
         AwakeningApi.setLevel(stack, spec.awakeningLevel());
         stack.set(ComponentTypeRegistry.GEM_POWER.get(), GemPowerComponent.createEmpty(true, true));
@@ -141,11 +150,20 @@ public final class UniqueTooltipExportController {
     }
 
     private static ExportSpec spec(String outputName, Supplier<? extends Item> item) {
-        return spec(outputName, item, MAX_AWAKENING);
+        return spec(outputName, item, MAX_AWAKENING, null);
     }
 
     private static ExportSpec spec(String outputName, Supplier<? extends Item> item, int awakeningLevel) {
-        return new ExportSpec(outputName, item, awakeningLevel);
+        return spec(outputName, item, awakeningLevel, null);
+    }
+
+    private static ExportSpec spec(String outputName, Supplier<? extends Item> item, Identifier awakeningRoute) {
+        return spec(outputName, item, MAX_AWAKENING, awakeningRoute);
+    }
+
+    private static ExportSpec spec(String outputName, Supplier<? extends Item> item, int awakeningLevel,
+                                   Identifier awakeningRoute) {
+        return new ExportSpec(outputName, item, awakeningLevel, awakeningRoute);
     }
 
     private static void sendMessage(String message, boolean actionBar) {
@@ -155,7 +173,8 @@ public final class UniqueTooltipExportController {
         }
     }
 
-    private record ExportSpec(String outputName, Supplier<? extends Item> item, int awakeningLevel) {
+    private record ExportSpec(String outputName, Supplier<? extends Item> item, int awakeningLevel,
+                              Identifier awakeningRoute) {
     }
 
     private static final class ExportMessages implements TooltipExportListener {
