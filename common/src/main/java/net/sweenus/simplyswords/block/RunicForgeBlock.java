1b0906b7b3db9afe32bb075baed31c64ddcb7649
Sweenus <sweeney.a.james@gmail.com>
2026-08-21 16:58:51 +1000

    Additions - Add Runic Forge assets

--- common/src/main/java/net/sweenus/simplyswords/block/RunicForgeBlock.java
+++ common/src/main/java/net/sweenus/simplyswords/block/RunicForgeBlock.java
@@ -4,26 +4,55 @@ import com.mojang.serialization.MapCodec;
 import dev.architectury.registry.menu.MenuRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.state.StateManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.sweenus.simplyswords.screen.RunicForgeScreenHandler;
 
-public class RunicForgeBlock extends Block {
+import java.util.List;
+
+public class RunicForgeBlock extends HorizontalFacingBlock {
     public static final MapCodec<RunicForgeBlock> CODEC = createCodec(RunicForgeBlock::new);
 
     public RunicForgeBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
-    protected MapCodec<? extends Block> getCodec() {
+    protected MapCodec<? extends HorizontalFacingBlock> getCodec() {
         return CODEC;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context,
+                              List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.literal(""));
+        tooltip.add(Text.translatable("block.simplyswords.runic_forge_description")
+                .formatted(Formatting.GRAY));
     }
 
     @Override
